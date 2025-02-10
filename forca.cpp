ae428bdcb38e40885f73587df192b00d057fc81f
Karol <karolsalvador91@gmail.com>
2025-02-10 19:05:40 -0300

    criação projeto

---
+++ forca.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //entrada e saida de dados
+#include <string> //biblioteca para suportar strings
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    string palavra_secreta = "MELANCIA";
+
+    cout <<palavra_secreta << endl;
+
+}
