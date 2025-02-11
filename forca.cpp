f6e08f2024caf7af3e3201f9e5d9cc86684bf9ff
Karol <karolsalvador91@gmail.com>
2025-02-11 15:28:21 -0300

    Criação da função letra_existe para verificar se o chute consta na palavra e do laço de repetição para sinalizar se a letra consta na plavra

--- forca.cpp
+++ forca.cpp
@@ -2,11 +2,33 @@
 #include <string> //biblioteca para suportar strings
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute) {
+    for( int i = 0; i < PALAVRA_SECRETA.size(); i++) {
+        if(chute == PALAVRA_SECRETA[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
-    string palavra_secreta = "MELANCIA";
+    bool nao_acertou = true;
+    bool nao_enforcou = true;
 
-    cout <<palavra_secreta << endl;
+    while(nao_acertou && nao_enforcou) {
+        char chute;
+
+        cin >> chute;
+
+        if(letra_existe(chute)) {
+            cout << "Você acertou, seu chute está na palavra." << endl;
+        } else {
+            cout << "Você errou, seu chute não está na palavra." << endl;
+        }
+    }
 
 }
