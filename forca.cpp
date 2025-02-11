e22885d85ca42ae8bd0d472f90058a4f24d4b78c
Karol <karolsalvador91@gmail.com>
2025-02-11 15:52:56 -0300

    inserido o map para varrer as letras que constam na palavra e imprimir isso para o usuário

--- forca.cpp
+++ forca.cpp
@@ -1,8 +1,10 @@
 #include <iostream> //entrada e saida de dados
 #include <string> //biblioteca para suportar strings
+#include <map> // inclui biblioteca do map
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe(char chute) {
     for(char letra : PALAVRA_SECRETA) {
@@ -13,6 +15,8 @@ bool letra_existe(char chute) {
     return false;
 }
 
+
+
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
@@ -20,9 +24,20 @@ int main() {
     bool nao_enforcou = true;
 
     while(nao_acertou && nao_enforcou) {
-        char chute;
+        for(char letra : PALAVRA_SECRETA) {
+            if(chutou[letra]) {
+                cout << letra << " ";
 
+            } else {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
+        char chute;
         cin >> chute;
+
+        chutou[chute] = true;
 
         if(letra_existe(chute)) {
             cout << "Você acertou, seu chute está na palavra." << endl;
